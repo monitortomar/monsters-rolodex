0494b0835277292e9abb84d4a5383e7c0feed72a
ManminderTomar1223 <manminder@limitinfinity.org>
2020-05-21 13:14:41 +0530

    Updates

---
+++ precache-manifest.6d31492bcb6920fd0c2c91ecd7069e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f2772e826c3c7b4a801269ad8507ed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aba05f90e3ad391ed7dc",
+    "url": "/monsters-rolodex/static/css/main.05211138.chunk.css"
+  },
+  {
+    "revision": "b81419f85758868b9ced",
+    "url": "/monsters-rolodex/static/js/2.62d78548.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.62d78548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba05f90e3ad391ed7dc",
+    "url": "/monsters-rolodex/static/js/main.d80becc5.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);

bd1181a0bcf124d4da82a1a4c2d62d7854927707
ManminderTomar1223 <manminder@limitinfinity.org>
2020-05-21 13:15:52 +0530

    files for gihub pages

---
+++ src/components/search-box/search-box.components.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import "./search-box.styles.css"
+
+export const SearchBox = ({placeHolder, handelChange}) => (
+    <input 
+    className='search'
+    type='search' 
+    placeholder={placeHolder}
+    onChange={handelChange}
+    />
+)
